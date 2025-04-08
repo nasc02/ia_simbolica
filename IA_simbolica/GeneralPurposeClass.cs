7d18160f9b78d459de1239a9b92eb4e078bf5fd2
nasc02 <henriqueaugusto840@gmail.com>
2025-04-08 19:53:38 -0300

    test inital version of algorithm

---
+++ IA_simbolica/GeneralPurposeClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ia_simbolica
+{
+    public class GeneralPurposeClass
+    {
+        public GeneralPurposeClass() { }
+
+        public int AcheOMelhorCaminhoParaORobo(int[,] matriz, int robo) {
+            int energiaGasta = 0;
+            int m0length = matriz.GetLength(0);
+            int m1length = matriz.GetLength(1);
+
+            //posição inicial do robo é sempre [0,0]
+            int x = 0, y = 0;
+
+            //supondo matriz perfeita inicialmente
+            while (x < m0length - 1) // se movendo para baixo até a última linha
+            {
+                x++;
+                energiaGasta++;
+            }
+
+            while (y < m1length - 1) // se movendo para a direita até a última coluna
+            {
+                y++;
+                energiaGasta++;
+            }
+
+            return energiaGasta;
+        }
+
+
+        public int[,] GerarMatrizAleatoria()
+        {
+            Random r = new Random();
+
+            int[,] matriz = new int[10, 10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                matriz[i, 0] = r.Next(20);
+
+                Console.Write(matriz[i, 0] + " ");
+
+                for (int j = 0; j < 10; j++)
+                {
+                    matriz[i, j] = r.Next(20);
+                    Console.Write(matriz[i, j] + " ");
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+            return matriz;
+        }
+    }
+}
---
+++ IA_simbolica/Program.cs
@@ -0,0 +1,47 @@
+﻿using ia_simbolica;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        GeneralPurposeClass g = new GeneralPurposeClass();
+
+        int[,] matriz = new int[10, 10];
+        int robo = 99; //numero aleatorio acima de 20
+        int energia = 50;
+
+        //possiveis movimentos
+        // [i-1, j] -> cima
+        // [i+1, j] -> baixo
+        // [i, j+1] -> direita
+        // [i-1, j-1] -> esquerda
+
+        //matriz ex
+        //0 0 1 2 3 4 5 6 7 8 9
+        //1 0 1 2 3 4 5 6 7 8 9
+        //2 0 1 2 3 4 5 6 7 8 9
+        //3 0 1 2 3 4 5 6 7 8 9
+        //4 0 1 2 3 4 5 6 7 8 9
+        //5 0 1 2 3 4 5 6 7 8 9
+        //6 0 1 2 3 4 5 6 7 8 9
+        //7 0 1 2 3 4 5 6 7 8 9
+        //8 0 1 2 3 4 5 6 7 8 9
+        //9 0 1 2 3 4 5 6 7 8 9
+        //matriz fixa para teste esboço inicial
+        //estou utilizando um numero randomico ate 20 pensando que talvez possamos usar alguns numero fixos para representar posições obscuras
+        matriz = g.GerarMatrizAleatoria();
+        List<int> obscurePositionsRepresentative = new List<int> { 5, 10, 15, 20 };
+
+        energia -= g.AcheOMelhorCaminhoParaORobo(matriz, robo);
+
+        Console.WriteLine($"energia final do robo no teste  {energia}");
+
+        //teste de movimento supondo o caminho perfeito
+
+
+
+
+    }
+
+}
